5e6bfd14c235b7653e23bea1d99600ebadda7200
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2024-10-22 22:48:16 -0600

    Remove github pages config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://techgems.github.io',
+	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
