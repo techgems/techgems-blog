c8f5f65447eaf97d4435cbcd96ea80c67b0cf991
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2024-10-30 14:39:59 -0600

    Add a projects page.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
+	site: "https://techgems.net",
 	integrations: [mdx(), sitemap()],
 });
