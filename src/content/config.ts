ac4b497a98b732e4f1511cb5be7045582cc84c3d
Carlos Jimenez <cjimenezber@gmail.com>
2026-03-04 14:31:50 -0700

    Add images and fix images for blog pages.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,13 +4,13 @@ const blog = defineCollection({
 	
 	type: 'content',
 	// Type-check frontmatter using a schema
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
 		date: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		heroImage: image().optional(),
 		published: z.boolean().optional(),
 		category: z.string().optional(),
 		usePlaceholderHeroInList: z.boolean().default(true),
